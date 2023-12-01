5578c31943d5f15a08611fb554d91771352c695b
rye141200 <126208685+rye141200@users.noreply.github.com>
2023-12-01 22:44:35 +0200

    Add files via upload

---
+++ Error Handling/ErrorHandler.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+public class ErrorHandler{
+    // IF ANY METHOD PROTOTYPE GOT CHANGED, LIST THE CHANGES IN THE "methods.txt" FILE 
+    // UNIT TEST ALL THE METHODS
+
+    /**************************************************/
+    /**************************************************/
+    /* 
+        Tasks: 1) Implement detectError method
+               2) Implement correctError method
+               3) Implement containsError method
+    */
+    /**************************************************/
+    /**************************************************/
+
+
+    public boolean containsError(){
+
+        return true; //remove this line before starting!!!
+    }
+
+    public ArrayList<String> detectError(){
+        /* 
+            Task:
+            - Implement a method that finds the mistakes in the XML file, store the lines with the mistakes in an ArrayList of strings and return it
+        */
+
+        //TODO
+
+        return new ArrayList<>();  //remove this line before starting!!!
+    }
+
+    public void correctError(){
+        /* 
+            Task:
+            - Implement a method that corrects the mistakes by writing into a new file, and asks the user where to store it
+            - Use the method detectError() to get the info about the lines containing the mistakes 
+         */
+
+        //TODO
+    }
+
+}
