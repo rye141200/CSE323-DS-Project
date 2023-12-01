f9f89110d5da952f16736bf66ad212868f3bbad4
Yousef Shawky <112232675+thedarkevil987@users.noreply.github.com>
2023-12-01 23:54:22 +0200

    Update TreeNode.java

--- Parsing and JSON/TreeNode.java
+++ Parsing and JSON/TreeNode.java
@@ -1,3 +1,37 @@
-public class TreeNode {
+import java.util.ArrayList;
 
+public class TreeNode {
+    private String TagName;
+    private String TagValue;
+    private ArrayList<TreeNode> Children;
+    
+    public TreeNode(String TagName, String TagValue, ArrayList<TreeNode> Children) {
+        this.TagName = TagName;
+        this.TagValue = TagValue;
+        this.Children = Children;
+    }
+
+    public String getTagName() {
+        return TagName;
+    }
+
+    public String getTagValue() {
+        return TagValue;
+    }
+
+    public ArrayList<TreeNode> getChildren() {
+        return Children;
+    }
+
+    public void setTagName(String TagName) {
+        this.TagName = TagName;
+    }
+
+    public void setTagValue(String TagValue) {
+        this.TagValue = TagValue;
+    }
+
+    public void addChildren(TreeNode Node) {
+        this.Children.add(Node);
+    }
 }
