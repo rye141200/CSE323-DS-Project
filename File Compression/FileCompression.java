33420e75d296faadb8a24598a4af7be6a2faaf5d
rye141200 <thecityhunterhd@gmail.com>
2023-12-01 23:05:31 +0200

    File compression tasks

---
+++ File Compression/FileCompression.java
@@ -0,0 +1,17 @@
+public class FileCompression{
+    // IF ANY METHOD PROTOTYPE GOT CHANGED, LIST THE CHANGES IN THE "methods.txt" FILE 
+    // UNIT TEST ALL THE METHODS
+
+    /**************************************************/
+    /**************************************************/
+    /* 
+        Tasks: 1) Implement compressXML method, compress the file removing the /n and making all lines stick to each other
+    */
+    /**************************************************/
+    /**************************************************/
+
+    public void compressXML(){
+        //TODO
+        
+    }
+}
