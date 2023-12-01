0663f51b751bbf1995c88ad541b1120e7b78d9a5
rye141200 <126208685+rye141200@users.noreply.github.com>
2023-12-01 22:56:59 +0200

    Add files via upload

---
+++ Formatting(Prettier)/Formatting.java
@@ -0,0 +1,17 @@
+public class Formatting {
+    // IF ANY METHOD PROTOTYPE GOT CHANGED, LIST THE CHANGES IN THE "methods.txt" FILE 
+    // UNIT TEST ALL THE METHODS
+
+    /**************************************************/
+    /**************************************************/
+    /* 
+        Tasks: 1) Implement formatXML method that handles identation in the given XML file, write into a new XML file and ask the user
+                where to store it in their own directory
+    */
+    /**************************************************/
+    /**************************************************/
+    public void formatXML(){
+        
+        //TODO
+    }
+}
