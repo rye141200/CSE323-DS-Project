ae1473ed7c3e76f4729e62fee765d0db1dc5b586
FathyAbdlhady <141967506+FathyAbdlhady@users.noreply.github.com>
2023-12-03 14:00:26 +0200

    Update FileCompression.java
    adding minify method

--- File Compression/FileCompression.java
+++ File Compression/FileCompression.java
@@ -8,7 +8,8 @@ public class FileCompression{
     /**************************************************/
     /**************************************************/
     /* 
-        Tasks: 1) Implement compressXML method, compress the file removing the /n and making all lines stick to each other
+        Tasks: 1) Implement minifyXML_Json method, minify the file removing the /n and spaces and making all lines stick to each other
+               2) Implement compress method, compress the file by encoding
     */
     /**************************************************/
     /**************************************************/
@@ -20,6 +21,74 @@ public class FileCompression{
             FileSample.fileSampleSelector();
         }
     */
+
+    public static String minify(ArrayList<String> lines)
+	{
+		String str = "",endString = "";
+		boolean prevChar,nextChar;
+		int length,start,end;
+		for(String line : lines)
+		{
+			length = line.length();    
+        	start = 0;
+			end = 0;    
+        	char val[] = line.toCharArray();
+
+			while( end < length )
+			{
+				start = end;
+				while( (start < length) && (val[start] <= ' ') )
+				{
+					start++;
+				}
+
+				end = start;
+
+				while( (end < length) &&  (val[end] != ' ') )
+				{
+					end++;
+				}
+				
+				str = str + line.substring(start, end) + " ";
+				
+				end++;
+			}
+		}
+		start = 0;
+		length = str.length();
+		end = str.indexOf(" ",start);
+		while( end != -1 )
+		{
+			prevChar = false;
+			nextChar = false;
+			if( (end-1 >= 0) && (end+1 < length) )
+			{
+				prevChar = Character.isLetterOrDigit(str.charAt(end-1));
+				nextChar = Character.isLetterOrDigit(str.charAt(end+1));
+				if( (!prevChar) || (!nextChar) )
+				{
+					endString = endString + str.substring(start, end);
+				}
+				else
+				{
+					endString = endString + str.substring(start, end+1);
+				}
+			}
+			else
+			{
+				endString = endString + str.substring(start, end+1);
+			}
+
+			start = end+1;
+			if(start >= length)
+			{
+				break;
+			}
+			end = str.indexOf(" ",start);
+		}
+		return endString;
+	}
+
     
     public void compressXML(){
         //TODO
