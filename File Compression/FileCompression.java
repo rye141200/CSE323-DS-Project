65e4ad7eaf7af7f77244b62d3010e8e5ce931d1c
AntiHexCode <131706131+AntiHexCode@users.noreply.github.com>
2023-12-07 23:44:10 +0200

    Added compressFile() and decompressFile() to FileCompression.java
    Added two functions responsible for compressing or decompressing XML and JSON files.

--- File Compression/FileCompression.java
+++ File Compression/FileCompression.java
@@ -92,16 +92,101 @@ public class FileCompression{
 		return endString;
 	}
 
-    // Use the following line of code to get parsed file:
-    // FileSampleEnhanced.readFileParsed(); -> Returns ArrayList<String> of parsed lines of file
-    /********************EXAMPLE**************************************************/
-    /*  
-        public static void compressXML() throws Exception{
-            FileSampleEnhanced.readFileParsed();
+	
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+	
+    // Author: Abdallah Mohamed (@AntiHexCode)
+    // Functions: compressFile(String fileType):void, decompressFile():void
+    // to compress and decompress XML or JSON file
+
+	
+    // Data Fields
+    static ArrayList<String> linesParsed = FileSampleEnhanced.readFileParsed();
+    static HashMap <String, String> encodingMap = new HashMap <String, String>();
+    static HashMap <String, String> decodingMap = new HashMap <String, String>();
+	
+    
+    // Compresses XML or JSON file, doesn't return anything
+    // it compresses the lines in the linesParsed ArrayList
+    public static void compressFile(String fileType){
+
+        String start;
+        String end;
+        
+        // using unicode characters to encode
+	// note that we can change it if we need more characters to encode with
+        int code = 65;
+        
+        // determining the start and the end of the encodded part of the line
+        // depends on the file format (XML or JSON)
+        if (fileType.toLowerCase().contains("json")){
+            start = "\"";
+            end = ":";
         }
-    */
-    public static void compressXML() throws Exception{
-        //TODO
-        ArrayList<String> linesParsed = FileSampleEnhanced.readFileParsed(); //Use this list in compressing the file, good luck!
+        else {
+            start = "<";
+            end = ">";
+        }
+        
+        for (int i = 0; i < linesParsed.size(); i++){
+            
+            String line = linesParsed.get(i);
+            int numberOfTags = ("begin"+line+"end").split(end).length - 1;
+            
+            // extracting the part we want to encode
+            while (numberOfTags != 0){
+                
+                if (linesParsed.get(i).contains(start) && linesParsed.get(i).contains(end)){
+                    int startingIndex = line.indexOf(start);
+                    int endingIndex = line.indexOf(end, startingIndex);
+                    String tag = line.substring(startingIndex, endingIndex + 1);
+
+                    // if it wasn't encodded before, then we will get a unique value for it and store it
+                    // and we will encode it using that unique value
+                    if (!encodingMap.containsKey(tag)){
+                        encodingMap.put(tag, String.valueOf((char)code));
+                        decodingMap.put(String.valueOf((char)code), tag);
+                        linesParsed.set(i, line.replace(tag, String.valueOf((char)code)));
+                        code++;
+                    }
+                    // but if it was encodded before then it must be in the encodingMap
+                    // so, we will use the same unique value we used before to encode it
+                    else{
+                        linesParsed.set(i, line.replace(tag, encodingMap.get(tag)));
+                    }
+                } 
+                line = linesParsed.get(i);
+                numberOfTags--; 
+	    }  
+        } 
+	    
     }
+
+    
+    // Decompresses a XML or JSON that was encodded with the compressFile function
+    // it decompresses the lines in the linesParsed ArrayList that was already compressed
+    public static void decompressFile(){
+        
+        for (int i = 0; i < linesParsed.size(); i++){
+            
+            String line = linesParsed.get(i);
+            
+            for (String key : decodingMap.keySet()){
+		    
+            	// for every key you find, replace it with the right tag
+                while (line.contains(key)){
+                    linesParsed.set(i, line.replace(key, decodingMap.get(key)));
+                    line = linesParsed.get(i);
+                }
+		    
+            }
+        }
+	    
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+	
 }
