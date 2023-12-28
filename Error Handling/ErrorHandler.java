583b2e93c9bf1d98c0ffb7961e77c076df17f8b4
Omar Saleh <omarsaleh12216@gmail.com>
2023-12-28 03:32:16 +0200

    Update ErrorHandler.java

--- Error Handling/ErrorHandler.java
+++ Error Handling/ErrorHandler.java
@@ -1,6 +1,8 @@
-import java.io.*;
-import java.util.Scanner;
+package com.example.demo.ErrorHandling;
 import java.util.ArrayList;
+
+import static com.example.demo.ErrorHandling.DetectionAndCorrectionMethods.*;
+
 public class ErrorHandler{
     // IF ANY METHOD PROTOTYPE GOT CHANGED, LIST THE CHANGES IN THE "methods.txt" FILE 
     // UNIT TEST ALL THE METHODS
@@ -23,53 +25,57 @@ public class ErrorHandler{
         }
     */
 
-    public boolean containsError(String path){
-
-        ArrayList Errors = detectError(path) ; 
-        if(Errors.size() == 0 ) return false ; 
-       else  return true; 
+    public static boolean containsError(ArrayList<String> lines) throws Exception {
+    return detectErrorsPrecise.containsError(lines);
     }
 
-     public static ArrayList<Integer>detectError(String path)throws IOException{
-            ArrayList<Integer> detectionAll = new ArrayList<>();
-            ////////////////////////////////////////////
-            ArrayList<String> parsedTags = parseTagsFromFile(path);
-            //////////////////////////////////////////////
 
-            ////////////////////////////////////////////
-            ArrayList<String>messages1=detectTagsErrors(parsedTags);
-            ArrayList<Integer>detect1 = pickLastNumbers(messages1);
-            ///////////////////////////////////////////
+    // should be called at first
+    public static ArrayList<Integer>detectError(StringBuilder XMLText)throws Exception{
+        ArrayList<Integer> detectionAll = new ArrayList<>();
+        ////////////////////////////////////////////
+        ArrayList<String> parsedTags = parseTagsFromFile(XMLText);
 
-            /////////////////////////////////////////
-            ArrayList<String>messages2=detectMissingClosingTag(parsedTags);
-            ArrayList<Integer>detect2 = pickLastNumbers(messages2);
-            //////////////////////////////////////////////
-            detectionAll.addAll(detect1);
-            detectionAll.addAll(detect2);
-            return detectionAll;
-        }
+        //////////////////////////////////////////////
 
+        ////////////////////////////////////////////
+        ArrayList<String>messages1=detectTagsErrors(parsedTags);
+        ArrayList<Integer>detect1 = pickLastNumbers(messages1);
+        ///////////////////////////////////////////
 
-
-  public static void correctError(String path) throws IOException
-        {
-            ////////////////////////////////////////////
-            ArrayList<String> parsedTags = parseTagsFromFile(path);
-            //////////////////////////////////////////////
-
-            /////////////////////////////////////////////
-            ArrayList<String> data1 = detectTagsErrors(parsedTags) ;
-            correctTagsErrors(data1,path);
-            ///////////////////////////////////////
-
-            ///////////////////////////////////////////
-            ArrayList<String>data2=detectMissingClosingTag(parsedTags);
-            correctMissingClosingTag(path,data2);
-            /////////////////////////////////////////
-        }
+        /////////////////////////////////////////
+        ArrayList<String>messages2=detectMissingClosingTag(parsedTags);
+        ArrayList<Integer>detect2 = pickLastNumbers(messages2);
+        //////////////////////////////////////////////
+        detectionAll.addAll(detect1);
+        detectionAll.addAll(detect2);
+        return detectionAll;
+    }
 
 
 
+    public static ArrayList<String> correctError(StringBuilder XMLText) throws Exception
+    {
+        ////////////////////////////////////////////
+        ArrayList<String> parsedTags = parseTagsFromFile(XMLText);
+        //////////////////////////////////////////////
 
+        /////////////////////////////////////////////
+        ArrayList<String> data1 = detectTagsErrors(parsedTags) ;
+        correctTagsErrors(data1,parsedTags);
+        ///////////////////////////////////////
+
+        ///////////////////////////////////////////
+        ArrayList<String>data2=detectMissingClosingTag(parsedTags);
+        correctMissingClosingTag(parsedTags,data2);
+        /////////////////////////////////////////
+        return parsedTags;
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        //System.out.println(detectError("sample.xml"));
+        //correctError("sample.xml");
+    }
 }
