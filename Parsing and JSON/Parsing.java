f4da834993b261ded10c0d0c18681fe6b6311a20
Omar Saleh <omarsaleh12216@gmail.com>
2023-12-28 03:35:00 +0200

    Update Parsing.java

--- Parsing and JSON/Parsing.java
+++ Parsing and JSON/Parsing.java
@@ -1,3 +1,5 @@
+package com.example.demo.Parsing;
+
 import java.util.Stack;
 import java.util.ArrayList;
 /*****************************/
@@ -10,47 +12,44 @@ import java.util.ArrayList;
 /*****************************/
 
 public class Parsing {
-    public static  void parseXML() throws Exception{
-        //INITIALIZATIONS
-        ArrayList<String> lines = FileSampleEnhanced.readFileParsed();
+    public static  Tree parseXML(ArrayList<String> lines) throws Exception{
         int currentLineCounter = 0;
         Tree XMLTree = new Tree(null);
         Stack<TreeNode> scopeStack = new Stack<>();
 
         for(String line: lines){
-            //1) Case of root node (BASIS CONDITION)
-            if(currentLineCounter == 0){
-                TreeNode root = new TreeNode(line.substring(1,line.length()-1),lines.get(currentLineCounter+1).contains("<")?null:lines.get(currentLineCounter+1));
+            //1) Case of root node (BASIS CONDITION) 
+            if(!line.isEmpty() && currentLineCounter == 0){
+                TreeNode root = new TreeNode(line.substring(1,line.length()-1),lines.get(currentLineCounter+1).contains("<") ? null:lines.get(currentLineCounter+1),null);
                 XMLTree.setRoot(root);
                 scopeStack.push(root);
             }
-            //2) Case of opening tag
-            if(line.contains("<") && line.contains(">") && !line.contains("/")){
-                ArrayList<String> tagNameValue = new ArrayList<>();
-                TreeNode node;
-                if((lines.get(currentLineCounter+1)).contains("<"))
-                {
-                    tagNameValue.add(line.substring(1,line.length()-1));
-                    tagNameValue.add(null);
+            else{
+                //2) Case of opening tag
+                if(!line.isEmpty() && line.charAt(0) == '<' && line.charAt(1) != '/'){
+                    ArrayList<String> tagNameValue = new ArrayList<>();
+                    TreeNode node;
+                    if((lines.get(currentLineCounter+1)).contains("<"))
+                    {
+                        tagNameValue.add(line.substring(1,line.length()-1));
+                        tagNameValue.add(null);
+                    }
+                    else{
+                        tagNameValue.add(line.substring(1,line.length()-1));
+                        tagNameValue.add(lines.get(currentLineCounter+1));
+                    }
+                    node = new TreeNode(tagNameValue.get(0),tagNameValue.get(1),scopeStack.peek());
+                    XMLTree.addNode(node,scopeStack.peek());
+                    scopeStack.add(node);
                 }
-                else{
-                    tagNameValue.add(line.substring(1,line.length()-1));
-                    tagNameValue.add(lines.get(currentLineCounter+1));
-                }
-                node = new TreeNode(tagNameValue.get(0),tagNameValue.get(1));
-                XMLTree.addNode(node,scopeStack.peek());
-                scopeStack.add(node);
+                //3) Case of closed tag
+                else if(!line.isEmpty() && line.charAt(0) == '<' && line.charAt(1) == '/')
+                    scopeStack.pop();
+                
             }
-            //3) Case of closed tag
-            else if(line.contains("<") &&line.contains(">") && line.contains("/"))
-                scopeStack.pop();
-            
             currentLineCounter++;
         }
-        XMLTree.printTree();        
-    }
-    public static void main(String[] args) throws Exception{
-        parseXML();
+        return XMLTree;       
     }
 }
 
