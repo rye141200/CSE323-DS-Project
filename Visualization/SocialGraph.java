2b3864b63591a8a1854395c076aa2e65294f248c
Yousef Shawky <112232675+thedarkevil987@users.noreply.github.com>
2023-12-28 08:17:26 +0200

    Update SocialGraph.java

--- Visualization/SocialGraph.java
+++ Visualization/SocialGraph.java
@@ -25,28 +25,5 @@ public class SocialGraph {
     public ArrayList<UserNode> getUsers(){
         return nodes;
     }
-    public static void main(String[] args){
-
-        UserNode OS = new UserNode("OS Chan", "1");
-        UserNode singerKun = new UserNode("Singer-Kun", "2");
-        UserNode Gaymer = new UserNode("Gaymer", "3");
-
-        OS.addFollower(Gaymer);
-        OS.addFollower(singerKun);
-        Post OSPost =new Post("I love to be a dickhead");
-        OSPost.addTopics("Bullshit");
-        OS.addPost(OSPost);
-
-        singerKun.addFollower(Gaymer);
-        singerKun.addFollower(OS);
-        Post singerKunPost = new Post("I am very awesome!");
-        singerKunPost.addTopics("The phenonemenon of OS Chan loving to lick feets");
-        singerKun.addPost(singerKunPost);
-
-        //shawkyChan.addNode(OS);
-        //shawkyChan.addNode(singerKun);
-        //shawkyChan.addNode(Gaymer);
-
-        //shawkyChan.printGraph();
-    }
+    
 }
